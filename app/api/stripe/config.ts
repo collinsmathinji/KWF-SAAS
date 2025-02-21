d506b4a6947ff700ee2965b6de6cdb787489a85b
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2025-02-21 09:15:03 -0800

    changes

--- app/api/stripe/config.ts
+++ app/api/stripe/config.ts
@@ -1,13 +1,12 @@
 
 import Stripe from "stripe"
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripeSecretKey = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
 if (!stripeSecretKey) {
-  console.log("stripesecret",stripeSecretKey)
+  console.log("stripesecret", stripeSecretKey)
   throw new Error("Missing required environment variable: STRIPE_SECRET_KEY")
-}else(
+} else {
   console.log("✅ STRIPE_SECRET_KEY is present:", stripeSecretKey)
-)
-
+}
 
 export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: "2025-01-27.acacia",
